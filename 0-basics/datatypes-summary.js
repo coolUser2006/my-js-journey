1bc405be1ff9b0cae52aa4e2db4809ec2ad99b37
coolUser2006 <156958756+coolUser2006@users.noreply.github.com>
2025-07-09 15:09:34 +0000

    change in location of file

---
+++ 0-basics/datatypes-summary.js
@@ -0,0 +1,24 @@
+// Primitive
+//7 types: String,boolean,null,undefined,symbol,bigInt,number
+const score = 100
+const scoreValue = 100.3
+const isloggedin = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123 ')
+const anotherId = Symbol('123')
+console.log(id===anotherId);
+//constbigNumber= 3455647789897904789
+//Reference(Non primitive)
+//Array, objects, functions
+const heros= ["tiger","aditya seal","siddharth malhotra"];
+let myObj={
+    name:"Himani",
+    age:19,
+}
+const myFunction=function(){
+    console.log("heelo javascript");
+
+}
+console.log(typeof anotherId);  
+
