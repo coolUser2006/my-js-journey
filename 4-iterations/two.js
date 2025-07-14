9a906d7f88cef371d560c83c1c68a29bb71f7bec
coolUser2006 <156958756+coolUser2006@users.noreply.github.com>
2025-07-14 14:08:28 +0000

    do and while loop

---
+++ 4-iterations/two.js
@@ -0,0 +1,18 @@
+//while loop
+let i = 0;
+while (i <= 10) {
+    console.log("Value of i is " + i);
+    i++;
+}
+let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let index = 0;
+while (index < myArray.length) {
+    const element = myArray[index];
+    console.log(element);
+    index++;
+}   
+let points =11
+do {
+    console.log("Value of points is " + points);
+    points++;
+} while (points <= 10);
