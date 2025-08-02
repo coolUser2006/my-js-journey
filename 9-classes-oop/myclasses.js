8bcbf719618662c2382ab3313cb1bf8e7ade5357
coolUser2006 <156958756+coolUser2006@users.noreply.github.com>
2025-08-02 17:24:19 +0000

    myclaases

---
+++ 9-classes-oop/myclasses.js
@@ -0,0 +1,30 @@
+//es6
+// ES6
+
+
+
+const chai = new User("chai", "chai@gmail.com", "123")
+
+console.log(chai.encryptPassword());
+console.log(chai.changeUsername());
+
+// behind the scene
+
+function User(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+
+const tea = new User("tea", "tea@gmail.com", "123")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
